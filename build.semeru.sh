f2ab075e417a6a1772fe92be07c01c43ed928946
jdmcclur <jdmcclur@us.ibm.com>
2024-11-21 16:29:16 -0500

    build semeru more efficiently

--- build.semeru.sh
+++ build.semeru.sh
@@ -1,1 +1,2 @@
-podman build --ulimit nofile=65535:65535 -t quarkus-semeru-flightservice -f Dockerfile.quarkus.semeru  --cap-add=ALL --security-opt seccomp=unconfined --cpu-quota=100000 -m 1g .
+podman build --ulimit nofile=65535:65535 -t quarkus-semeru-flightservice-step1 -f Dockerfile.quarkus.semeru.step1
+podman build -t quarkus-semeru-flightservice -f Dockerfile.quarkus.semeru.step2  --cap-add=ALL --security-opt seccomp=unconfined --cpu-quota=100000 -m 1g --no-cache .
